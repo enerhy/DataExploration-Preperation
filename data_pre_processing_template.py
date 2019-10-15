e6054fa9588b2230664d5528c6ceda5eba40ce41
Spasov <51121651+enerhy@users.noreply.github.com>
2019-10-15 12:25:54 +0300

    Update data_pre_processing_template.py

--- data_pre_processing_template.py
+++ data_pre_processing_template.py
@@ -27,6 +27,8 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+#we specify the column to be encoded
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 # Encoding the Dependent Variable
